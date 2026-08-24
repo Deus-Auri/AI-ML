36988a875a78a409e2643ed865a3d2ba78d1f44a
Deus Auri <mr.fool736@gmail.com>
2026-08-24 19:46:13 +0000

    Aug 25, 2026, 1:16 AM

---
+++ Python-Basics/Calculator.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+modulus=a%b
+floor_division=a//b
+power=a**b
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Floor Division:", floor_division)
+print("Power:", power)
