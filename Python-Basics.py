1ef1542100e914522c8460c812048a18eb1a92e0
Deus Auri <mr.fool736@gmail.com>
2026-08-24 18:56:38 +0000

    Aug 25, 2026, 12:26 AM

--- Python-Basics.py
+++ Python-Basics.py
@@ -1,1 +1,14 @@
-print("hello world")
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+modulus=a%b
+floor_division=a//b
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Floor Division:", floor_division)
