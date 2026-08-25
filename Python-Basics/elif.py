d0824bde8e7926efa748b91b9b0210bf09f6ce90
Deus Auri <mr.fool736@gmail.com>
2026-08-26 00:00:45 +0530

    learned elif

---
+++ Python-Basics/elif.py
@@ -0,0 +1,8 @@
+marks = 20
+if marks < 30:
+    print("Fail")
+else:
+    if (marks >= 31) and (marks <= 70):
+        print("average")
+    else:
+        print("excellent")
---
+++ Python-Basics/elif2.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your marks: "))
+if marks < 30:
+    print("Fail")
+elif (marks >= 30) and (marks <= 70):
+    print("Average")
+else:
+    print("Excellent")
+
+# Coolest Code (Used in Google)
+
+marks = int(input("Enter your marks: "))
+if marks < 30:
+    print("Fail")
+elif marks <= 70:
+    print("Average")
+else:
+    print("Excellent")
